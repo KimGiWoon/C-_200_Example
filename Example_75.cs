201ba8e4b12cf19db9e099c41f2edeaa14fc4d48
KimGiWoon <rldns20704@naver.com>
2026-05-25 20:56:38 +0900

    [Feat] Add Example_75
    75번 예제 문제 추가

--- Example_75.cs
+++ Example_75.cs
@@ -11,12 +11,31 @@ namespace C__200_Example
     {
         static void Main(string[] args)
         {
-            // 이렇게 간다
-            if (true)
-            {
+            int number = 0;
 
+            Console.WriteLine("팩토리얼 구하기!");
+            Console.Write("팩토리얼을 구할 숫자를 입력하세요 : ");
+
+            // 입력 숫자 체크
+            while (!int.TryParse(Console.ReadLine(), out number) || (number < 1 || number > 15))
+            {
+                Console.Write("1이상 15이하의 숫자를 입력하세요 : ");
             }
 
+            FactorialCal(number);
+        }
+
+        // 입력 숫자 팩토리얼 계산
+        private static void FactorialCal(int number)
+        {
+            int fact = 1;
+            
+            for (int i = 1; i <= number; i++)
+            {
+                fact *= i;
+            }
+
+            Console.WriteLine($"{number}! = {fact:N0}");
         }
     }
 }
