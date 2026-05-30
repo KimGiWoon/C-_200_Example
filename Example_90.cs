9bb883653b3522689a7af7b8e50959804cce5e66
KimGiWoon <rldns20704@naver.com>
2026-05-30 21:37:12 +0900

    [Feat] Add Example_90 ,91
    90, 91번 예제 문제 추가

---
+++ Example_90.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// 가변길이 매개변수 params의 사용 방법
+namespace C__200_Example
+{
+    internal class Example_90
+    {
+        static void Main(string[] args)
+        {
+            PrintIntParams(1, 2, 3, 4);
+            PrintObjectParams(1, 1.234, 'a', "Test");
+            PrintObjectParams();
+
+            int[] intArray = { 5, 6, 7, 8, 9 };
+            PrintIntParams(intArray);
+
+            object[] objectArray = { 1, 2.45, 'b', "Test", "Again" };
+            // PrintIntParams(objectArray); // 에러
+            PrintObjectParams(objectArray);
+            PrintObjectParams(intArray);
+        }
+
+        // 가변 정수형 매개변수 출력
+        public static void PrintIntParams(params int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        // 가변 오브젝트형 매개변수 출력 (오브젝트는 int, double, char, string의 값을 모두 가질 수 있음)
+        public static void PrintObjectParams(params object[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Example_91.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 선택적 인수와 명명된 인수
+namespace C__200_Example
+{
+    internal class Example_91
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyPower(4, 2));
+            Console.WriteLine(MyPower(4));
+            Console.WriteLine(MyPower(3, 4));
+
+            Console.WriteLine(Area(5, 6));
+            Console.WriteLine(Area(w : 7, h : 3));  // 매개변수 이름으로 매칭
+            Console.WriteLine(Area(h : 10, w : 6));
+        }
+
+        // 입력수 제곱
+        static int MyPower(int x, int y = 2)    // y의 기본값 2 설정
+        {
+            int result = 1;
+
+            for (int i = 0; i < y; i++)
+            {
+                result *= x;
+            }
+
+            return result;
+        }
+
+        // 사각형의 넓이 구하기
+        static int Area(int h, int w)
+        {
+            return h * w;
+        }
+    }
+}
