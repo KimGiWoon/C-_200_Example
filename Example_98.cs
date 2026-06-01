ce9139a6eec8af43dfd2fa28a1df851cc0008788
KimGiWoon <rldns20704@naver.com>
2026-06-01 23:18:53 +0900

    [Feat] Add Example_98
    98번 예제 문제 추가

---
+++ Example_98.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Stack<T> 구현
+namespace C__200_Example
+{
+    class MyStack<T>
+    {
+        const int maxSize = 10;
+        private int top;
+
+        private T[] arr = new T[maxSize];
+
+        public MyStack()
+        {
+            top = 0;
+        }
+
+        // 스택에 값 넣기
+        public void Push(T value)
+        {
+            if (top < arr.Count())
+            {
+                arr[top] = value;
+                top++;
+            }
+            else
+            {
+                Console.WriteLine("Stack Full");
+                return;
+            }
+        }
+
+        // 스택에 값 내보내기
+        public T Pop()
+        {
+            if (top > 0)
+            {
+                top--;
+                return arr[top];
+            }
+            else
+            {
+                Console.WriteLine("Stack Empty");
+                return default(T);
+            }
+        }
+    }
+}
